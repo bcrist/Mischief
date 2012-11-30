2c492bcec5ad651231fee7842ec4fa5b55c4457e
Ben Crist <ben@magicmoremagic.com>
2012-11-30 16:24:59 -0600

    fixed stb_image fopen() warnings

--- OpenGL3_3Test/contrib/include/stb_image.h
+++ OpenGL3_3Test/contrib/include/stb_image.h
@@ -181,7 +181,7 @@
 
 #ifndef STBI_NO_STDIO
 
-#if defined(_MSC_VER) && _MSC_VER >= 0x1400
+#if defined(_MSC_VER) && _MSC_VER >= 1400
 #define _CRT_SECURE_NO_WARNINGS // suppress bogus warnings about fopen()
 #endif
 
